3a522e1a82aaa698a2addff62988185e6b2d4520
githinjisamson1 <githinjisamson148@gmail.com>
2023-12-14 05:43:25 +0300

    Complete Object Relationship Methods

--- lib/models.py
+++ lib/models.py
@@ -5,6 +5,10 @@ from sqlalchemy import ForeignKey, Table, Column, Integer, String, DateTime
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import sessionmaker
+
+Session = sessionmaker()
+session = Session()
 
 engine = create_engine('sqlite:///migrations_test.db')
 
@@ -23,14 +27,22 @@ class Restaurant(Base):
     # relationship
     reviews = relationship("Review", back_populates="restaurant")
 
+    # association with customers through reviews
     customers = association_proxy('reviews', 'customer',
-                              creator=lambda cu: Review(customer=cu))
-    
+                                  creator=lambda cu: Review(customer=cu))
+
     # representation
     def __repr__(self):
         return f'Restaurant(restaurant_id={self.restaurant_id}, ' + \
             f'name={self.name}, ' + \
             f'price={self.price})'
+
+    # !OBJECT RELATIONSHIP METHODS
+    def reviews(self):
+        return self.reviews
+
+    def customers(self):
+        return self.customers
 
 
 class Customer(Base):
@@ -42,17 +54,25 @@ class Customer(Base):
     first_name = Column(String())
     last_name = Column(String())
 
-    # relationship
+    # relationship with Review on customer
     reviews = relationship("Review", back_populates="customer")
-    
+
+    # association with restaurants throught reviews
     restaurants = association_proxy('reviews', 'restaurant',
-                                  creator=lambda res: Review(restaurant=res))
+                                    creator=lambda res: Review(restaurant=res))
 
     # representation
     def __repr__(self):
         return f'Customer(customer_id={self.customer_id}, ' + \
             f'first_name={self.first_name}, ' + \
             f'last_name={self.last_name})'
+
+    # !OBJECT RELATIONSHIP METHODS
+    def reviews(self):
+        return self.reviews
+
+    def restaurants(self):
+        return self.restaurants
 
 
 class Review(Base):
@@ -64,7 +84,7 @@ class Review(Base):
     restaurant_id = Column(Integer(), ForeignKey("restaurants.restaurant_id"))
     customer_id = Column(Integer(), ForeignKey("customers.customer_id"))
 
-    # relationship
+    # relationship with Restaurant on reviews
     restaurant = relationship("Restaurant", back_populates="reviews")
     customer = relationship("Customer", back_populates="reviews")
 
@@ -74,9 +94,23 @@ class Review(Base):
             f'restaurant_id={self.restaurant_id}, ' + \
             f'customer_id={self.customer_id})'
 
+    # !OBJECT RELATIONSHIP METHODS
+    def customer(self):
+        return self.customer
 
+    def restaurant(self):
+        return self.restaurant
+
+
+# !not related to the above
 # TODO: understand association_proxy
 '''
 Notice in the Game and User models that we have added an association_proxy (imported from sqlalchemy.ext.associationproxy) to refer to the many-to-many related table. This states that there is an association through the reviews table's game or user column. The creator argument takes a function (an anonymous lambda function in this case) which accepts a game or user and returns a review for that game or user. This review has, in a sense, created the relationship between the game and user.
 
 '''
+
+
+# instances
+res1 = Restaurant()
+customer1 = Customer()
+review1 = Review()
